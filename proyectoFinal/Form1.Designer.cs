64ba9d931877eb93e9167785ecbf82dc3401fbfe
wood2120 <kristwood11@gmail.com>
2024-11-14 20:13:25 -0600

    Add project files.

---
+++ proyectoFinal/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace proyectoFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picR3 = new System.Windows.Forms.PictureBox();
+            this.picR2 = new System.Windows.Forms.PictureBox();
+            this.picR5 = new System.Windows.Forms.PictureBox();
+            this.picR4 = new System.Windows.Forms.PictureBox();
+            this.picR1 = new System.Windows.Forms.PictureBox();
+            this.picC1 = new System.Windows.Forms.PictureBox();
+            this.picD2 = new System.Windows.Forms.PictureBox();
+            this.picD1 = new System.Windows.Forms.PictureBox();
+            this.picM4 = new System.Windows.Forms.PictureBox();
+            this.picM3 = new System.Windows.Forms.PictureBox();
+            this.picM6 = new System.Windows.Forms.PictureBox();
+            this.picM5 = new System.Windows.Forms.PictureBox();
+            this.picM7 = new System.Windows.Forms.PictureBox();
+            this.picM2 = new System.Windows.Forms.PictureBox();
+            this.picM1 = new System.Windows.Forms.PictureBox();
+            this.picM8 = new System.Windows.Forms.PictureBox();
+            this.btnPago = new System.Windows.Forms.Button();
+            this.btnReservar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picD2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picD1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(952, 639);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picR3
+            // 
+            this.picR3.Location = new System.Drawing.Point(95, 99);
+            this.picR3.Name = "picR3";
+            this.picR3.Size = new System.Drawing.Size(57, 52);
+            this.picR3.TabIndex = 1;
+            this.picR3.TabStop = false;
+            // 
+            // picR2
+            // 
+            this.picR2.Location = new System.Drawing.Point(95, 479);
+            this.picR2.Name = "picR2";
+            this.picR2.Size = new System.Drawing.Size(57, 52);
+            this.picR2.TabIndex = 2;
+            this.picR2.TabStop = false;
+            // 
+            // picR5
+            // 
+            this.picR5.Location = new System.Drawing.Point(440, 99);
+            this.picR5.Name = "picR5";
+            this.picR5.Size = new System.Drawing.Size(57, 52);
+            this.picR5.TabIndex = 3;
+            this.picR5.TabStop = false;
+            // 
+            // picR4
+            // 
+            this.picR4.Location = new System.Drawing.Point(259, 99);
+            this.picR4.Name = "picR4";
+            this.picR4.Size = new System.Drawing.Size(57, 52);
+            this.picR4.TabIndex = 4;
+            this.picR4.TabStop = false;
+            // 
+            // picR1
+            // 
+            this.picR1.Location = new System.Drawing.Point(259, 479);
+            this.picR1.Name = "picR1";
+            this.picR1.Size = new System.Drawing.Size(57, 52);
+            this.picR1.TabIndex = 5;
+            this.picR1.TabStop = false;
+            // 
+            // picC1
+            // 
+            this.picC1.Location = new System.Drawing.Point(440, 441);
+            this.picC1.Name = "picC1";
+            this.picC1.Size = new System.Drawing.Size(57, 52);
+            this.picC1.TabIndex = 6;
+            this.picC1.TabStop = false;
+            // 
+            // picD2
+            // 
+            this.picD2.Location = new System.Drawing.Point(627, 441);
+            this.picD2.Name = "picD2";
+            this.picD2.Size = new System.Drawing.Size(57, 52);
+            this.picD2.TabIndex = 7;
+            this.picD2.TabStop = false;
+            // 
+            // picD1
+            // 
+            this.picD1.Location = new System.Drawing.Point(791, 441);
+            this.picD1.Name = "picD1";
+            this.picD1.Size = new System.Drawing.Size(57, 52);
+            this.picD1.TabIndex = 8;
+            this.picD1.TabStop = false;
+            // 
+            // picM4
+            // 
+            this.picM4.Location = new System.Drawing.Point(570, 193);
+            this.picM4.Name = "picM4";
+            this.picM4.Size = new System.Drawing.Size(28, 27);
+            this.picM4.TabIndex = 9;
+            this.picM4.TabStop = false;
+            // 
+            // picM3
+            // 
+            this.picM3.Location = new System.Drawing.Point(617, 193);
+            this.picM3.Name = "picM3";
+            this.picM3.Size = new System.Drawing.Size(28, 27);
+            this.picM3.TabIndex = 10;
+            this.picM3.TabStop = false;
+            // 
+            // picM6
+            // 
+            this.picM6.Location = new System.Drawing.Point(617, 69);
+            this.picM6.Name = "picM6";
+            this.picM6.Size = new System.Drawing.Size(28, 27);
+            this.picM6.TabIndex = 11;
+            this.picM6.TabStop = false;
+            // 
+            // picM5
+            // 
+            this.picM5.Location = new System.Drawing.Point(570, 69);
+            this.picM5.Name = "picM5";
+            this.picM5.Size = new System.Drawing.Size(28, 27);
+            this.picM5.TabIndex = 12;
+            this.picM5.TabStop = false;
+            // 
+            // picM7
+            // 
+            this.picM7.Location = new System.Drawing.Point(656, 69);
+            this.picM7.Name = "picM7";
+            this.picM7.Size = new System.Drawing.Size(28, 27);
+            this.picM7.TabIndex = 13;
+            this.picM7.TabStop = false;
+            // 
+            // picM2
+            // 
+            this.picM2.Location = new System.Drawing.Point(656, 193);
+            this.picM2.Name = "picM2";
+            this.picM2.Size = new System.Drawing.Size(28, 27);
+            this.picM2.TabIndex = 14;
+            this.picM2.TabStop = false;
+            // 
+            // picM1
+            // 
+            this.picM1.Location = new System.Drawing.Point(706, 193);
+            this.picM1.Name = "picM1";
+            this.picM1.Size = new System.Drawing.Size(28, 27);
+            this.picM1.TabIndex = 15;
+            this.picM1.TabStop = false;
+            // 
+            // picM8
+            // 
+            this.picM8.Location = new System.Drawing.Point(706, 69);
+            this.picM8.Name = "picM8";
+            this.picM8.Size = new System.Drawing.Size(28, 27);
+            this.picM8.TabIndex = 16;
+            this.picM8.TabStop = false;
+            // 
+            // btnPago
+            // 
+            this.btnPago.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPago.BackgroundImage")));
+            this.btnPago.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPago.FlatAppearance.BorderSize = 0;
+            this.btnPago.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPago.Location = new System.Drawing.Point(958, 44);
+            this.btnPago.Name = "btnPago";
+            this.btnPago.Size = new System.Drawing.Size(91, 82);
+            this.btnPago.TabIndex = 17;
+            this.btnPago.UseVisualStyleBackColor = true;
+            this.btnPago.Click += new System.EventHandler(this.btnPago_Click);
+            // 
+            // btnReservar
+            // 
+            this.btnReservar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnReservar.BackgroundImage")));
+            this.btnReservar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReservar.FlatAppearance.BorderSize = 0;
+            this.btnReservar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReservar.Location = new System.Drawing.Point(1077, 44);
+            this.btnReservar.Name = "btnReservar";
+            this.btnReservar.Size = new System.Drawing.Size(91, 82);
+            this.btnReservar.TabIndex = 18;
+            this.btnReservar.UseVisualStyleBackColor = true;
+            this.btnReservar.Click += new System.EventHandler(this.btnReservar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1026, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 221);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Tarifas:\r\n\r\nVehiculo particular:\r\n$2 la hora\r\n$20 todo el dia\r\n\r\nMoto:\r\n$1.5 la h" +
+    "ora\r\n$15 todo el dia\r\n\r\nCamion:\r\n$4 la hora\r\n$40 todo el dia\r\n\r\nDiscapacitado:\r\n" +
+    "$1 la hora\r\n$10 todo el dia\r\n";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(1029, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 82);
+            this.button1.TabIndex = 20;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1180, 637);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReservar);
+            this.Controls.Add(this.btnPago);
+            this.Controls.Add(this.picM8);
+            this.Controls.Add(this.picM1);
+            this.Controls.Add(this.picM2);
+            this.Controls.Add(this.picM7);
+            this.Controls.Add(this.picM5);
+            this.Controls.Add(this.picM6);
+            this.Controls.Add(this.picM3);
+            this.Controls.Add(this.picM4);
+            this.Controls.Add(this.picD1);
+            this.Controls.Add(this.picD2);
+            this.Controls.Add(this.picC1);
+            this.Controls.Add(this.picR1);
+            this.Controls.Add(this.picR4);
+            this.Controls.Add(this.picR5);
+            this.Controls.Add(this.picR2);
+            this.Controls.Add(this.picR3);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picR1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picD2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picD1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picM8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picR3;
+        private System.Windows.Forms.PictureBox picR2;
+        private System.Windows.Forms.PictureBox picR5;
+        private System.Windows.Forms.PictureBox picR4;
+        private System.Windows.Forms.PictureBox picR1;
+        private System.Windows.Forms.PictureBox picC1;
+        private System.Windows.Forms.PictureBox picD2;
+        private System.Windows.Forms.PictureBox picD1;
+        private System.Windows.Forms.PictureBox picM4;
+        private System.Windows.Forms.PictureBox picM3;
+        private System.Windows.Forms.PictureBox picM6;
+        private System.Windows.Forms.PictureBox picM5;
+        private System.Windows.Forms.PictureBox picM7;
+        private System.Windows.Forms.PictureBox picM2;
+        private System.Windows.Forms.PictureBox picM1;
+        private System.Windows.Forms.PictureBox picM8;
+        private System.Windows.Forms.Button btnPago;
+        private System.Windows.Forms.Button btnReservar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ proyectoFinal/Reservar.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace proyectoFinal
+{
+    partial class Reservar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reservar));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPago = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(113, 21);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(780, 286);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(126, 353);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(145, 20);
+            this.txtPlaca.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Placa vehiculo";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(333, 352);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(353, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tipo de vehiculo";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(525, 353);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(541, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tipo de campo";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(694, 356);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Hora";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(766, 357);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(42, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Dia";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(712, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tipo de tarifa";
+            // 
+            // btnPago
+            // 
+            this.btnPago.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPago.BackgroundImage")));
+            this.btnPago.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPago.FlatAppearance.BorderSize = 0;
+            this.btnPago.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPago.Location = new System.Drawing.Point(393, 413);
+            this.btnPago.Name = "btnPago";
+            this.btnPago.Size = new System.Drawing.Size(91, 82);
+            this.btnPago.TabIndex = 18;
+            this.btnPago.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(555, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 68);
+            this.button1.TabIndex = 19;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Reservar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 581);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPago);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.listView1);
+            this.Name = "Reservar";
+            this.Text = "Reservar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPago;
+        private System.Windows.Forms.Button button1;
+    }
+}
